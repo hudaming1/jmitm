ae656a7f6492680e106313c6443dca4502c50e31
hudaming1 <512657752@qq.com>
2020-08-25 09:53:02 +0800

    修改代码

--- wiretiger-core/src/main/java/org/hum/wiretiger/core/pipe/event/EventHandler.java
+++ wiretiger-core/src/main/java/org/hum/wiretiger/core/pipe/event/EventHandler.java
@@ -1,9 +1,12 @@
 package org.hum.wiretiger.core.pipe.event;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.hum.wiretiger.api.core.EventListener;
+import org.hum.wiretiger.api.core.WireTigerPipe;
+import org.hum.wiretiger.api.core.WiretigerSession;
 import org.hum.wiretiger.core.pipe.bean.PipeHolder;
 import org.hum.wiretiger.core.session.bean.WtSession;
 
@@ -21,49 +24,69 @@ public class EventHandler {
 
 	public void fireConnectEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onConnect(pipe);
+			listener.onConnect(convert(pipe));
 		}
 	}
 
 	public void fireReadEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onPipeStatusChange(pipe);
+			listener.onPipeStatusChange(convert(pipe));
 		}
 	}
 
 	public void fireReceiveEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onPipeStatusChange(pipe);
+			listener.onPipeStatusChange(convert(pipe));
 		}
 	}
 
 	public void fireForwardEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onPipeStatusChange(pipe);
+			listener.onPipeStatusChange(convert(pipe));
 		}
 	}
 
 	public void fireFlushEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onPipeStatusChange(pipe);
+			listener.onPipeStatusChange(convert(pipe));
 		}
 	}
 
 	public void fireDisconnectEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onDisconnect(pipe);
+			listener.onDisconnect(convert(pipe));
 		}
 	}
 
 	public void fireErrorEvent(PipeHolder pipe) {
 		for (EventListener listener : listeners) {
-			listener.onError(pipe);
+			listener.onError(convert(pipe));
 		}
 	}
 
-	public void fireNewSessionEvent(PipeHolder pipeHolder, WtSession session) {
+	public void fireNewSessionEvent(PipeHolder pipe, WtSession session) {
 		for (EventListener listener : listeners) {
-			listener.onNewSession(pipeHolder, session);
+			listener.onNewSession(convert(pipe), convert(session));
 		}
+	}
+	
+	private WireTigerPipe convert(PipeHolder holder) {
+		WireTigerPipe pipeVo = new WireTigerPipe();
+		InetSocketAddress source = (InetSocketAddress) holder.getClientChannel().remoteAddress();
+		InetSocketAddress target = (InetSocketAddress) holder.getServerChannel().remoteAddress();
+		pipeVo.setSourceHost(source.getHostName());
+		pipeVo.setSourcePort(source.getPort());
+		pipeVo.setTargetHost(target.getHostName());
+		pipeVo.setTargetPort(target.getPort());
+		pipeVo.setProtocol(holder.getProtocol());
+		pipeVo.setPipeId(holder.getId() + "");
+		
+		return pipeVo;
+	}
+	
+	private WiretigerSession convert(WtSession wtSession) {
+		WiretigerSession session = new WiretigerSession();
+		
+		return session;
 	}
 }
