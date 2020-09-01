bc2fcc5d88ed09bf5e785d27650832bd266b783c
hudaming1 <512657752@qq.com>
2020-09-01 09:40:35 +0800

    修改代码

--- wiretiger-console/src/main/java/org/hum/wiretiger/console/http/helper/ConsoleHelper.java
+++ wiretiger-console/src/main/java/org/hum/wiretiger/console/http/helper/ConsoleHelper.java
@@ -5,6 +5,7 @@ import org.hum.wiretiger.console.http.vo.WiretigerSessionListVO;
 import org.hum.wiretiger.proxy.facade.event.WiretigerPipe;
 import org.hum.wiretiger.proxy.facade.event.WiretigerSession;
 import org.hum.wiretiger.proxy.facade.lite.WiretigerFullPipe;
+import org.hum.wiretiger.proxy.facade.lite.WiretigerSimpleSession;
 import org.hum.wiretiger.proxy.pipe.enumtype.Protocol;
 
 public class ConsoleHelper {
@@ -33,6 +34,14 @@ public class ConsoleHelper {
 		vo.setProtocol(item.getProtocol() == null ? Protocol.UNKNOW.getDesc() : item.getProtocol().getDesc());
 		vo.setStatus(item.getStatus().getDesc());
 		return vo;
+	}
+
+	public static WiretigerSessionListVO parse2WtSessionListVO(WiretigerSimpleSession session) {
+		WiretigerSessionListVO listVO = new WiretigerSessionListVO();
+		listVO.setSessionId(session.getSessionId() + "");
+		listVO.setUri(session.getUri());
+		listVO.setResponseCode(session.getResponseCode());
+		return listVO;
 	}
 
 	public static WiretigerSessionListVO parse2WtSessionListVO(WiretigerSession session) {
