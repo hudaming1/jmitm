1e3c93b3031cd3e223c05d62099c53561e62ba62
hudaming1 <512657752@qq.com>
2020-09-04 15:37:28 +0800

    漏提交

--- wiretiger-common/src/main/java/org/hum/wiretiger/common/constant/HttpConstant.java
+++ wiretiger-common/src/main/java/org/hum/wiretiger/common/constant/HttpConstant.java
@@ -8,4 +8,6 @@ public class HttpConstant {
 	public static final String HTML_NEWLINE = "<br />";
 	public static final int DEFAULT_HTTP_PORT = 80;
 	public static final int DEFAULT_HTTPS_PORT = 443;
+	public static final String ConnectedLine = "HTTP/1.1 200 Connection established\r\n\r\n";
+	public static final String HTTPS_HANDSHAKE_METHOD = "CONNECT";
 }
