856268f73931d2f2b10357ea3ed3728e1ab78ba9
hudaming1 <512657752@qq.com>
2020-09-05 10:22:38 +0800

    mock-api 伪代码

--- wiretiger-start/src/main/java/org/hum/wiretiger/WiretigerServerRun.java
+++ wiretiger-start/src/main/java/org/hum/wiretiger/WiretigerServerRun.java
@@ -1,5 +1,8 @@
 package org.hum.wiretiger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hum.wiretiger.console.common.listener.Console4WsListener;
 import org.hum.wiretiger.console.http.ConsoleServer;
 import org.hum.wiretiger.console.http.config.WtConsoleHttpConfig;
@@ -19,6 +22,7 @@ public class WiretigerServerRun {
 				config.setPort(52007);
 				config.setThreads(Runtime.getRuntime().availableProcessors());
 				config.setDebug(false);
+				config.setInterceptors(buildInterceptor());
 				
 				WtServerBuilder.init(config).addEventListener(new Console4WsListener()).build().start();
 			}
@@ -50,5 +54,46 @@ public class WiretigerServerRun {
 				}
 			}
 		}).start();
+	}
+	
+	public static List<Mock> buildMock() {
+		List<Mock> mocks = new ArrayList<>();
+		
+		Mock mock = new Mock();
+		/*** 根据Request拦截 ****/
+		InterceptorPicture picture4Req = new InterceptorPicture(InterceptorType.Request);
+		/*** 根据Response拦截 ****/
+		InterceptorPicture picture4Res = new InterceptorPicture(InterceptorType.Repsonse);
+		// 自定义拦截逻辑
+		picture4Res.eval((fullHttpRequest) -> {
+			
+		});
+		// 拦截request.header.Host
+		picture4Req.header("Host").equal("xxx");
+		picture4Req.header("Host").like("xxx");
+		// 通过自定义表达式拦截
+		picture4Req.header("Host").eval((headerVal)-> {
+			return headerVal + "XXX";
+		});
+		// 通过request.uri拦截
+		picture4Req.uri("xxx");
+		// 通过模糊匹配关键字(request.uri + request.header + request.body)拦截
+		picture4Req.keyword("xxx");
+		
+		// 对请求进行改造
+		criminal.rebuild(InterceptorType.Request).header("Host").modify("http://www.baidu.com");
+		criminal.rebuild(InterceptorType.Request).header("Host").modify((headerVal -> {
+			return headerVal + "XXX";
+		}));
+
+		// 对响应进行改造
+		criminal.rebuild(InterceptorType.Response).header("Host").modify("http://www.baidu.com");
+		criminal.rebuild(InterceptorType.Response).bdoy().modify((headerVal -> {
+			return headerVal + "XXX";
+		}));
+		
+		mock.setInterceporPicture(picture4Req);
+		mocks.add(mock);
+		return mocks;
 	}
 }
