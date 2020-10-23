4a1bc8cfb648d0739edc1aaf827701f4e6168294
hudaming1 <512657752@qq.com>
2020-10-23 10:40:03 +0800

    补充注释

--- wiretiger-proxy/src/main/java/org/hum/wiretiger/proxy/config/WiretigerCoreConfig.java
+++ wiretiger-proxy/src/main/java/org/hum/wiretiger/proxy/config/WiretigerCoreConfig.java
@@ -16,9 +16,16 @@ public class WiretigerCoreConfig {
 	private int threads = Runtime.getRuntime().availableProcessors() * 2;
 	//
 	private boolean isDebug;
-	//
+	/**
+	 * Mock清单：决定了对哪些「请求」或「响应」进行拦截，以及拦截后进行什么样的处理
+	 * 确保Interceptor和Rebuilder必须均至少出现一次
+	 */
 	private List<Mock> mockList = new ArrayList<Mock>();
-	//
+	/**
+	 * 是否对HTTPS协议进行解析（解析的前提是需要客户端安装WiretigerCA）
+	 * 如果客户端不希望Wiretiger对客户端引入CA，即可以将该值设为false，
+	 * 这样Wiretiger只拦截HTTP的请求和响应，对HTTPS请求和响不做解析，直接放行处理
+	 */
 	private boolean isParseHttps = true;
 	
 	public void addMock(Mock mock) {
--- wiretiger-proxy/src/main/java/org/hum/wiretiger/proxy/mock/Mock.java
+++ wiretiger-proxy/src/main/java/org/hum/wiretiger/proxy/mock/Mock.java
@@ -9,9 +9,13 @@ public class Mock {
 	
 	private static AtomicInteger idGenerator = new AtomicInteger(1);
 	private String id;
+	// 定义了拦截什么样的HTTP请求
 	private RequestInterceptor requestInterceptor;
+	// 定义了拦截什么样的HTTP响应
 	private ResponseInterceptor responseInterceptor;
+	// 对拦截到的HTTP请求进行重制，你可以修改HTTP请求报文的任意信息，包括URI、Header、以及Body部分
 	private RequestRebuilder requestRebuilder;
+	// 对拦截到的HTTP响应进行重制，你可以修改HTTP响应报文的任意信息，包括状态码、Header、以及Body部分
 	private ResponseRebuild responseRebuild;
 
 	public Mock(RequestInterceptor requestInterceptor, ResponseInterceptor responseInterceptor, RequestRebuilder requestRebuilder, ResponseRebuild responseRebuild) {
